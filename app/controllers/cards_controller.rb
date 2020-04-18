2244ce0773e2b4b598520ae67862255f3a232103
Leon Lee <jtllee1@gmail.com>
2020-04-18 21:42:41 +0100

    added routes and views

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,2 +1,6 @@
 class CardsController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index]
+
+  def index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :booster_sets, only: [:index] do
+    resources :booster_packs, only: [:index]
+  end
+  resources :cards, only: [:index]
 end
