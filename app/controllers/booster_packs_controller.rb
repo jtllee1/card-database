09d37f8543fdbcd1b6b9d166d98d1341da04e1de
Leon Lee <jtllee1@gmail.com>
2020-04-19 17:26:53 +0100

    sorted cards in booster pack

--- app/controllers/booster_packs_controller.rb
+++ app/controllers/booster_packs_controller.rb
@@ -2,6 +2,6 @@ class BoosterPacksController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
 
   def index
-    @booster_packs = BoosterPack.where(booster_set_id: params[:booster_set_id])
+    @booster_packs = BoosterPack.where(booster_set_id: params[:booster_set_id]).sort
   end
 end
