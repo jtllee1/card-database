08604da999e485b1d82fe5eb9bb726f8995c863e
Leon Lee <jtllee1@gmail.com>
2020-04-19 21:51:05 +0100

    added random pack opening feature

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :booster_sets, only: [:index] do
-    resources :booster_packs, only: [:index]
+    resources :booster_packs, only: [:index, :show]
   end
   resources :cards, only: [:show]
 end
